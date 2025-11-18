a8480dd6730a0eb4564eb709088ddee25a2a6f5f
cbertley3112 <cbertley3112@student-7.novalocal>
2025-11-18 00:43:58 -0600

    Implemented Verlet Velocity

---
+++ src/integrator.cpp
@@ -0,0 +1,16 @@
+void velocity_verlet(Particles& P,
+                     const std::vector<float>& ax,
+                     const std::vector<float>& ay,
+                     const std::vector<float>& az,
+                     float dt)
+{
+    for (size_t i = 0; i < P.N; i++) {
+        P.vx[i] += 0.5f * ax[i] * dt;
+        P.vy[i] += 0.5f * ay[i] * dt;
+        P.vz[i] += 0.5f * az[i] * dt;
+
+        P.x[i] += P.vx[i] * dt;
+        P.y[i] += P.vy[i] * dt;
+        P.z[i] += P.vz[i] * dt;
+    }
+}

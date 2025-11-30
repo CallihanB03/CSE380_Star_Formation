a6b395881ac8e173ee74f0b6866f941269fdc27e
cbertley3112 <cbertley3112@student-7.novalocal>
2025-11-29 22:39:54 -0600

    Includes subtraction

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -2,22 +2,6 @@
 
 #pragma once
 
-// struct Vec3 {
-//     float x, y, z;
-
-//     Vec3(float x_=0, float y_=0, float z_=0)
-//         : x(x_), y(y_), z(z_) {}
-
-//     Vec3 operator-(const Vec3& rhs) const {
-//         return Vec3(x - rhs.x, y - rhs.y, z - rhs.z);
-//     }
-
-//     float norm() const {
-//         return std::sqrt(x*x + y*y + z*z);
-//     }
-// };
-
-
 struct Vec3 {
     float x, y, z;
 
@@ -43,4 +27,9 @@ struct Vec3 {
     Vec3 operator/(float s) const {
         return Vec3(x/s, y/s, z/s);
     }
+
+    Vec3 operator-(const Vec3& v) const {
+    return Vec3(x - v.x, y - v.y, z - v.z);
+}
+
 };
